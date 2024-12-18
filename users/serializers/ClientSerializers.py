0e509ad1f98032cd388daa6cc67bc92f292cf5ff
majoiefaya <majoiefaya@gmail.com>
2024-12-18 16:36:25 +0100

    update user

--- users/serializers/ClientSerializers.py
+++ users/serializers/ClientSerializers.py
@@ -6,7 +6,7 @@ from users.models.Client import Client
 class ClientRequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
-        fields = ["email","password","firstName","lastName","dateOfBirth"]
+        fields = ["email","firstName","lastName","dateOfBirth"]
 
 class ClientResponseSerializer(serializers.ModelSerializer):
     class Meta:
